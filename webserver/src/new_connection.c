d61deb41d37fb50d54811f19d2f377b86c3b49c2
Emil Joensson <emil@emiljoensson.com>
2015-10-11 23:09:50 +0200

    URL validation security + get the requested content properly

--- webserver/src/new_connection.c
+++ webserver/src/new_connection.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>		// Includes a number of definitions of structures needed for sockets
 #include <sys/stat.h>		// stat() to obtain information about file
 #include <netinet/in.h>		// Constants and structures needed for internet domain addresses
+#include <limits.h> 		// PATH_MAX
 
 #include "error.h"
 #include "new_connection.h"
@@ -28,38 +29,74 @@ void new_connection(int sock) {
 	if (n < 0)
 		error("ERROR: failed to read from socket");
 
-	/* DEBUGGING , CONSOLE OUTPUT */
-	printf("Client Request: \n%s\n", message);
+	/* DEBUGGING */
+	printf("\nClient Request: \n%s\n", message);
 
-    /* Parse the client request */
+	/* Parse the client request */
 	char* RequestLine = strtok(message, "\n");
 	char* Req_Method = strtok(RequestLine, " ");
 	char* Req_Path = strtok(NULL, " ");
 	char* Req_Version = strtok(NULL, " ");
 	if (Req_Path[0] =='/' && strlen(Req_Path) == 1) // Sets path to /index.html if / is requested
-		Req_Path = "index.html";
+		Req_Path = "/index.html";
 
-	/* Maka a struct that stores the attributes of the file */
+	char *RootDir;
+	RootDir = getenv("PWD");
+	char AbsolutePath[100];
+	RootDir[strlen(RootDir)-9] = '\0'; // Parent folder of working directory
+
+	/* Appending Req_Path to our www root */
+	strcpy(AbsolutePath, RootDir);
+	strcat(AbsolutePath, "www");
+	/* We save this as it's our www-root. Will be useful for in URL validation */
+	int wwwRootLen = strlen(AbsolutePath);
+	char wwwRoot[wwwRootLen];
+	strcpy(wwwRoot, AbsolutePath);
+	/* Finally we append Req_Path to the www-root */
+	if (Req_Path[0] != '/') // Deal with trailing slash
+		strcat(AbsolutePath, "/");
+	strcat(AbsolutePath, Req_Path);
+
+	char PathBuf[PATH_MAX + 1]; // not sure about the "+ 1"
+	realpath(AbsolutePath, PathBuf); // Change relative path to absolute path, used for URL validation
+
+	/* Uncomment to debug realpath (comment reapath above) 
+	char *res = realpath(AbsolutePath, PathBuf);
+	if (res) {
+		printf("This source is at %s.\n", PathBuf);
+	} else {
+		perror("realpath");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("\nres: %s\n", res);
+	printf("\nwwwRoot: %s", wwwRoot);
+	printf("\nwwwRootLen: %d\n", wwwRootLen);
+	*/
+
+	/* Check if it's a valid request */ 
+	if (strncmp(PathBuf,wwwRoot,wwwRootLen) != 0) {
+		/* URL validation security */
+		valid = 0;
+	} else if (strncmp(Req_Method,"GET",3) != 0 && strncmp(Req_Method,"HEAD",4) != 0 && strncmp(Req_Method,"POST",4) != 0) {
+		/* Check if it's a valid HTTP 1.0 method, if not set valid to 0 else continues */
+		valid = 0;
+	} else if (strncmp(Req_Version,"HTTP/1.0",8) != 0 && strncmp(Req_Version,"HTTP/1.1",8) != 0) {
+		/* Check if it's not HTTP 1.0 or 1.1, if so set valid to 0, else continues */
+		valid = 0;
+	}
+
+	/* Make a struct that stores the attributes of the file */
 	struct stat attr;
 	int FileLength;
 	FILE *file;
-
-	/* Check if it's a valid HTTP 1.0 method, if not set valid to 0 else continues */
-	if (strncmp(Req_Method,"GET",3) != 0 && strncmp(Req_Method,"HEAD",4) != 0 && strncmp(Req_Method,"POST",4) != 0) {
-		valid = 0;
-	}
-
-	/* Check if it's not HTTP 1.0 or 1.1, if so set valid to 0, else continues */
-	if (strncmp(Req_Version,"HTTP/1.0",8) != 0 && strncmp(Req_Version,"HTTP/1.1",8) != 0) {
-		valid = 0;
-	}
 
 	int sendBody = 1;
 
 	if (valid == 0) { // If it's an invalid HTTP request
 		Resp_Header="HTTP/1.0 400 Bad Request";
 		snprintf(response,sizeof(response), "%s%s",Resp_Header,Resp_Server);
-		Req_Path = "400.html";
+		Req_Path = "/400.html";
 	} else { // If it's a valid HTTP request
 
 		/* Check request method and compose response accordingly */
@@ -67,9 +104,9 @@ void new_connection(int sock) {
 
 			/* Checking if we find the requested file */
 
-			if (access(Req_Path, F_OK) == 0 && access(Req_Path, R_OK) == 0) { // Found the file and can read (200 OK)
+			if (access(PathBuf, F_OK) == 0 && access(PathBuf, R_OK) == 0) { // Found the file and can read (200 OK)
 				/* Figuring out Content-Length (check length of file) */
-				stat(Req_Path, &attr);
+				stat(PathBuf, &attr);
 				FileLength = (int)attr.st_size;
 				/* Setting the headers */
 				Resp_Header="HTTP/1.0 200 OK";
@@ -80,26 +117,26 @@ void new_connection(int sock) {
 				if (strncmp(Req_Method,"HEAD",4) == 0) {
 					sendBody = 0;
 				}
-			} else if (access(Req_Path, F_OK) == 0 && access(Req_Path, R_OK) == -1) { // Found the file but can't read (403)
+			} else if (access(PathBuf, F_OK) == 0 && access(PathBuf, R_OK) == -1) { // Found the file but can't read (403)
 				Resp_Header="HTTP/1.0 403 Forbidden";
-				Req_Path = "403.html";
+				Req_Path = "/403.html";
 				snprintf(response,sizeof(response), "%s\nServer: %s",Resp_Header,Resp_Server);
 			} else { // Did NOT find the file (404 Not Found)
 				Resp_Header="HTTP/1.0 404 Not Found";
-				Req_Path = "404.html";
+				Req_Path = "/404.html";
 				snprintf(response,sizeof(response), "%s\nServer: %s",Resp_Header,Resp_Server);
 			}
 		} 
 
 		else if (strncmp(Req_Method,"POST",4) == 0 && valid == 1){ // If POST, return 501
 			Resp_Header="HTTP/1.0 501 Not Implemented";
-			Req_Path = "501.html";
+			Req_Path = "/501.html";
 			snprintf(response,sizeof(response), "%s\nServer: %s",Resp_Header,Resp_Server);
 		}
 
 		else { // If it's not GET, HEAD or POST, return 400
 			Resp_Header="HTTP/1.0 400 Bad Request";
-			Req_Path = "400.html";
+			Req_Path = "/400.html";
 			snprintf(response,sizeof(response), "%s\nServer: %s",Resp_Header,Resp_Server);
 		}
 	}
@@ -113,23 +150,33 @@ void new_connection(int sock) {
 	/* Sending the body (content of Req_Path) */
 	if (sendBody == 1) {
 
-		file = fopen(Req_Path, "r");
-		stat(Req_Path, &attr);
+		int finalPathLen = wwwRootLen + strlen(Req_Path);
+		char finalPath[finalPathLen];
+		strcpy(finalPath, wwwRoot);
+		if (Req_Path[0] != '/') // Deal with trailing slash
+			strcat(finalPath, "/");
+		strcat(finalPath, Req_Path);
+
+		/* DEBUG */
+		printf("\nfinalPath: %s\n", finalPath);
+
+		file = fopen(finalPath, "r");
+		stat(finalPath, &attr);
 		FileLength = (int)attr.st_size;
 
 		char sdbuf[8196];
-	    bzero(&sdbuf, sizeof(sdbuf));
-	    int bytes = 0;
-	    fseek(file, 0, SEEK_END);
-	    rewind(file);
-	    int line;
-	    write(sock,"\n\n",4); // Making two line breaks between headers and body
-	    while(bytes < FileLength){
-	        line = fread(sdbuf, 1, sizeof(sdbuf), file);
-	        bytes += line;
-	        write(sock, sdbuf, line);
-	        bzero(&sdbuf, sizeof(sdbuf));
-	    }
+		bzero(&sdbuf, sizeof(sdbuf));
+		int bytes = 0;
+		fseek(file, 0, SEEK_END);
+		rewind(file);
+		int line;
+		write(sock,"\n\n",4); // Making two line breaks between headers and body
+		while(bytes < FileLength){
+			line = fread(sdbuf, 1, sizeof(sdbuf), file);
+			bytes += line;
+			write(sock, sdbuf, line);
+			bzero(&sdbuf, sizeof(sdbuf));
+		}
 		fclose(file);
 	}
 }
